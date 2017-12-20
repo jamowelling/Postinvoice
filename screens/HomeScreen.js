b2e25e5647764745faf12cb2c90955a1f007523b
Noah Schutte <noahschutte@gmail.com>
2017-12-19 21:08:21 -0700

    routes work

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,12 +1,114 @@
 import React, { Component } from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
+import { DB_URL } from 'react-native-dotenv';
 
 class HomeScreen extends Component <{}> {
 
   onPress() {
+    console.log('DB_URL: ', DB_URL);
     const route = this.children.props.children;
-    alert(route);
+    console.log('route: ', route);
+    switch (route) {
+      case 'index': {
+        const url = `${DB_URL}/invoices`;
+        fetch(url, {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+          },
+          method: 'GET',
+        })
+        .then(response => response.json())
+        .then(responseJSON => {
+          if (responseJSON.errorMessage) {
+            alert(responseJSON.errorMessage);
+          } else {
+            console.log('responseJSON: ', responseJSON);
+          }
+        })
+        .catch(err => alert(err));
+      }
+        break;
+      case 'show':
+        fetch(`${DB_URL}/invoices/${1}`, {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+          },
+          method: 'GET'
+        })
+        .then(response => response.json())
+        .then(responseJSON => {
+          if (responseJSON.errorMessage) {
+            alert(responseJSON.errorMessage);
+          } else {
+            console.log('responseJSON: ', responseJSON);
+          }
+        })
+        .catch(error => console.error(error));
+        break;
+      case 'delete':
+        fetch(`${DB_URL}/invoices/${1}`, {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+          },
+          method: 'DELETE',
+        })
+        .then(response => response.json())
+        .then(responseJSON => {
+          if (responseJSON.errorMessage) {
+            alert(responseJSON.errorMessage);
+          } else {
+            console.log('responseJSON: ', responseJSON);
+          }
+        })
+        .catch(err => alert(err));
+        break;
+      case 'create': {
+        const date = new Date();
+        const supplierName = 'Company Z';
+        const invoiceNumber = '5555';
+        const total = 40.00;
+        const items = [
+          {
+            code: 'Cheese',
+            amount: 20.00
+          },
+          {
+            code: 'Glasses',
+            amount: 20.00
+          }
+        ];
+        fetch(`${DB_URL}/invoices`, {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json'
+          },
+          method: 'POST',
+          body: JSON.stringify({
+            date,
+            supplierName,
+            invoiceNumber,
+            total,
+            items,
+          })
+        })
+        .then(response => response.json())
+        .then(responseJSON => {
+          if (responseJSON.errorMessage) {
+            alert(responseJSON.errorMessage);
+          } else {
+            console.log('responseJSON: ', responseJSON);
+          }
+        })
+        .catch(error => console.error(error));
+        break;
+      }
+    }
   }
+
+
 
   render() {
     return (
@@ -19,11 +121,6 @@ class HomeScreen extends Component <{}> {
         <TouchableOpacity onPress={this.onPress} style={styles.button} >
           <Text>
             show
-          </Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={this.onPress} style={styles.button} >
-          <Text>
-            edit
           </Text>
         </TouchableOpacity>
         <TouchableOpacity onPress={this.onPress} style={styles.button} >
