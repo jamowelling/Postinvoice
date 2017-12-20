70715d327c90f589ee0424a6d5e01dadb0813245
Jameson Welling <jamowelling@gmail.com>
2017-12-19 18:36:59 -0700

    add logic for dealing with displaying the correct buttons on navbar

--- NavBar.js
+++ NavBar.js
@@ -1,22 +1,22 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 class NavBar extends Component <{}> {
+  determineIcon = (name) => {
+    return <Icon name={name} size={30} color="#222" />;
+  }
   render() {
     return (
       <View style={styles.container}>
-        <View style={styles.leftWrapper}>
-          <Text>
-            Back
-          </Text>
+        <View style={styles.button}>
+          {this.determineIcon('bars')}
         </View>
         <View style={styles.titleWrapper}>
           <Text>Title</Text>
         </View>
-        <View>
-          <Text>
-            Right
-          </Text>
+        <View style={styles.button}>
+          {this.determineIcon('plus')}
         </View>
       </View>
     );
@@ -26,11 +26,14 @@ class NavBar extends Component <{}> {
 const styles = {
   container: {
     flexDirection: 'row',
-    justifyContent: 'space-around',
+    justifyContent: 'space-between',
     alignItems: 'center',
     flex: 1,
     backgroundColor: '#eee',
   },
+  button: {
+    padding: 15,
+  }
 };
 
 export default NavBar;
