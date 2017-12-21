1fae7a7eece281a7c878ff54ed171f4827bb47e6
Jameson Welling <jamowelling@gmail.com>
2017-12-21 15:55:13 -0700

    responsive vendor selection, rudimentary

--- screens/AddInvoice/SelectVendorScreen.js
+++ screens/AddInvoice/SelectVendorScreen.js
@@ -5,9 +5,11 @@ import {
   TouchableOpacity,
   FlatList,
 } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 class SecondScreen extends Component <{}> {
   state = {
+    selectedVendor: '',
     vendorNames: [
       {
         key: 'natural',
@@ -21,26 +23,94 @@ class SecondScreen extends Component <{}> {
         key: 'freshpack',
         name: 'Freshpack Produce',
       },
+      {
+        key: '1',
+        name: 'Natural Wine Co.',
+      },
+      {
+        key: '2',
+        name: 'Elite Brands',
+      },
+      {
+        key: '3',
+        name: 'Freshpack Produce',
+      },
+      {
+        key: '4',
+        name: 'Natural Wine Co.',
+      },
+      {
+        key: '5',
+        name: 'Elite Brands',
+      },
+      {
+        key: '6',
+        name: 'Freshpack Produce',
+      },
+      {
+        key: '7',
+        name: 'Natural Wine Co.',
+      },
+      {
+        key: '8',
+        name: 'Elite Brands',
+      },
+      {
+        key: '9',
+        name: 'Freshpack Produce',
+      },
+      {
+        key: '10',
+        name: 'Natural Wine Co.',
+      },
+      {
+        key: '11',
+        name: 'Elite Brands',
+      },
+      {
+        key: '12',
+        name: 'Freshpack Produce',
+      },
     ]
   }
 
+  onPress = (selectedVendor) => {
+    this.setState({ selectedVendor });
+  }
+
   rendorVendor = ({ item }) => {
+    if (this.state.selectedVendor !== item.key){
+      return (
+        <TouchableOpacity
+          key={item.key}
+          style={styles.vendorContainer}
+          onPress={() => this.onPress(item.key)}
+        >
+          <Text style={styles.vendorTextStyle}>{item.name}</Text>
+        </TouchableOpacity>
+      );
+    }
     return (
-      <View key={item.key} style={styles.vendorContainer}>
+      <TouchableOpacity
+        key={item.key}
+        style={styles.selectedVendorContainer}
+        onPress={() => this.onPress(item.key)}
+      >
         <Text style={styles.vendorTextStyle}>{item.name}</Text>
-      </View>
+        <Icon name='check' size={30} color='green' style={styles.iconStyle} />
+      </TouchableOpacity>
     );
   }
 
   render() {
     return (
       <View style={{ flex: 1 }}>
-
         <View style={{ flex: 7 }}>
           <FlatList
-            style={{ flex: 1  }}
+            style={styles.vendorListContainer}
             data={this.state.vendorNames}
             renderItem={this.rendorVendor}
+            extraData={this.state}
           />
         </View>
 
@@ -61,9 +131,9 @@ class SecondScreen extends Component <{}> {
 const styles = {
   buttonContainer: {
     flexDirection: 'row',
-    flex: 2,
-    alignItems: 'center',
-    margin: 15,
+    flex: 1,
+    alignItems: 'flex-start',
+    padding: 15,
     justifyContent: 'space-around',
   },
   buttonStyle: {
@@ -73,14 +143,28 @@ const styles = {
     elevation: 1,
     borderRadius: 2
   },
+  vendorListContainer: {
+    flex: 1,
+  },
+  selectedVendorContainer: {
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
   vendorTextStyle: {
     fontSize: 26,
     margin: 6,
   },
   vendorContainer: {
+    backgroundColor: '#e9e9e9',
     borderBottomWidth: 0.5,
     borderColor: 'rgba(0,0,0,0.2)',
     flex: 1,
+  },
+  iconStyle: {
+    margin: 5,
+    alignSelf: 'center',
+    paddingRight: 25,
   }
 };
 
