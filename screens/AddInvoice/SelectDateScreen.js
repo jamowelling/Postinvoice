29d95a1fc0e4e97a2f5e5b872744daf40f0dfd4f
Jameson Welling <jamowelling@gmail.com>
2017-12-21 11:11:50 -0700

    date picker up and running

--- screens/AddInvoice/SelectDateScreen.js
+++ screens/AddInvoice/SelectDateScreen.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View, Dimensions } from 'react-native';
+
+import DatePicker from 'react-native-datepicker';
 
 class SelectDateScreen extends Component <{}> {
   constructor(props) {
     super(props);
     this.props.navigator.setOnNavigatorEvent(this.onNavigatorEvent.bind(this));
+    this.state = {
+      date: new Date().toLocaleDateString(),
+    };
   }
 
   onNavigatorEvent(event) {
@@ -18,10 +23,34 @@ class SelectDateScreen extends Component <{}> {
   }
   render() {
     return (
-      <View>
-        <Text>
-          Select Date Screen
-        </Text>
+      <View style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+        <DatePicker
+          style={{
+            width: Dimensions.get('window').width,
+            height: 400,
+            paddingBottom: 50,
+            justifyContent: 'center'
+          }}
+          androidMode='default'
+          customStyles={{
+            dateText: {
+              fontSize: 40,
+            },
+            dateInput: {
+              // height: 60,
+              borderColor: 'rgba(0,0,0,0.2)',
+              borderRadius: 15,
+              margin: 7,
+            }
+          }}
+          placeholder={this.state.date}
+          mode='date'
+          format='MM-DD-YYYY'
+          date={this.state.date}
+          confirmBtnText='Confirm'
+          cancelBtnText='Cancel'
+          onDateChange={date => this.setState({ date })}
+        />
       </View>
     );
   }
